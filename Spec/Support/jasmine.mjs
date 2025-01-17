380b0fe2091233c1bdbb38d1bafb89c7ac9310c6
AbdellahVinci <165916198+AbdellahVinci@users.noreply.github.com>
2025-01-17 02:05:49 +0100

    Update jasmine.mjs

--- Spec/Support/jasmine.mjs
+++ Spec/Support/jasmine.mjs
@@ -1,14 +1,14 @@
-export default {
-  spec_dir: "spec",
-  spec_files: [
+{
+  "spec_dir": "Spec",
+  "spec_files": [
     "**/*[sS]pec.?(m)js"
   ],
-  helpers: [
-    "helpers/**/*.?(m)js"
+  "helpers": [
+    "Support/**/*.?(m)js"
   ],
-  env: {
-    stopSpecOnExpectationFailure: false,
-    random: true,
-    forbidDuplicateNames: true
+  "env": {
+    "stopSpecOnExpectationFailure": false,
+    "random": true,
+    "forbidDuplicateNames": true
   }
 }
